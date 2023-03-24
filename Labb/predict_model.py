664f20036c8d80859bfb7c88cae96162098be290
vineela.nedunuri <vineela.nedunuri45@gmail.com>
2023-03-24 15:28:11 +0100

    save

--- Labb/predict_model.py
+++ Labb/predict_model.py
@@ -5,7 +5,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import  accuracy_score
@@ -46,7 +46,7 @@ def scale_features(scale_type='standard'):
  # Create pipelines
     pipe_logistic = Pipeline([("scaler", scaler), ("LR", LogisticRegression())])
     pipe_KNN = Pipeline([("scaler", scaler), ("KNN", KNeighborsClassifier())])
-    pipe_svm = Pipeline([("scaling", scaler), ("SVM", SVC())])
+    pipe_svm = Pipeline([("scaling", scaler), ("SVM", LinearSVC())])
     pipe_forest = Pipeline([("scaling", scaler), ("RF", RandomForestClassifier())])
     pipe_Gaussian = Pipeline([("scaling", scaler), ("NB", GaussianNB())])
 
@@ -54,7 +54,7 @@ def scale_features(scale_type='standard'):
     pipelines = {
         'Logistic Regression': pipe_logistic,
         'K-Nearest Neighbor': pipe_KNN,
-        'Support vector machines': pipe_svm,
+        'Support vector machines linear': pipe_svm,
         'Random Forest': pipe_forest,
         'Gaussian Naive Bayes': pipe_Gaussian
     }
@@ -65,7 +65,7 @@ def scale_features(scale_type='standard'):
 # Grid searchCV  for each model with their accuracy
 def grid_search(pipeline,param_grid, X_train, y_train, X_val, y_val, score_file):
 
-    grid_search= GridSearchCV(estimator=pipeline, param_grid=param_grid, scoring="accuracy", cv=3)
+    grid_search= GridSearchCV(estimator=pipeline, param_grid=param_grid, scoring="accuracy", cv=3,error_score='raise')
 
     # to fit that object to training data
     grid_search.fit(X_train, y_train)
@@ -85,7 +85,7 @@ def grid_search(pipeline,param_grid, X_train, y_train, X_val, y_val, score_file)
 
     # save the accuracy score to a file 
     with open(score_file, 'a') as f:
-        f.write(f'{type(pipeline).__name__}: {accuracy * 100:.2f}%\n')
+        f.write(f'{type(model).__name__}: {accuracy * 100:.2f}%\n')
     
     
     
